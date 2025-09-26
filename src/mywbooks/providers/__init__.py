baaa2db8b8401aaa8cca0d9a2e6a3bdae2724d78
Nokorot <torhaakon2012@hotmail.com>
2025-09-26 16:30:51 +0200

    Now provider modules are only loaded, when needed

--- src/mywbooks/providers/__init__.py
+++ src/mywbooks/providers/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
 import importlib
+import typing
 from enum import StrEnum
+
+## === Warning!! ===
+# When adding new providers. Both
+#   - A unique identifier most be added to the Provider class, and
+#   - The module name most be added to the _PROVIDER_MODULES dict
 
 
 # By writing this explicitly, it is earlier for the autocomplete
@@ -12,7 +18,7 @@ class Provider(StrEnum):
 
 
 # fmt: off
-_PROVIDER_MODULE: dict[str, str] = {
+_PROVIDER_MODULES: dict[str, str] = {
         Provider.ROYALROAD: "royalroad",
         # Provider.PATREON = "patreon"
         # Provider.WUXIAWORLD = "wuxiaworld"
@@ -20,25 +26,43 @@ _PROVIDER_MODULE: dict[str, str] = {
 # fmt: on
 
 
+class _ProviderInfo(typing.NamedTuple):
+    provider_key: str
+    module_name: str
+    short_name: str
+    provider_class: object
+
+
 def get_provider_by_key(key: str):
-    try:
-        return _get_providers_registery()[key]["provider_class"]
-    except KeyError:
+    # Store provider_register as function attribute
+    if not hasattr(get_provider_by_key, "initialized_value"):
+        setattr(
+            get_provider_by_key,
+            "provider_register",
+            {},
+        )
+    provider_register: dict[str, _ProviderInfo] = getattr(
+        get_provider_by_key, "provider_register"
+    )
+
+    # Check for provider in registry
+    pinfo = provider_register.get(key)
+    if pinfo:
+        return pinfo.provider_class
+
+    # If not loaded, look up module name
+    mname = _PROVIDER_MODULES.get(key)
+    if not mname:
         raise ValueError(f"No provider registered for {key}")
 
+    # Load module
+    print("Loading provider module for '{key}', at  '{mname}'")
 
-# ================= Code Gen  =================
-
-
-def _keypair_for(provider_key: str):
-    module_name = _PROVIDER_MODULE[provider_key]
-
-    module = importlib.import_module(f".{module_name}", package=__name__)
+    module = importlib.import_module(f".{mname}", package=__name__)
 
     def _get_assert_var(var_name, desc):
-        assert hasattr(
-            module, var_name
-        ), f"Module {provider_key} is missing {var_name} [{desc}]"
+        if not hasattr(module, var_name):
+            raise RuntimeError(f"Module {key} is missing {var_name} [{desc}]")
         return getattr(module, var_name)
 
     # key = _get_assert_var("PROVIDER_KEY", "Identification Key")
@@ -53,23 +77,14 @@ def _keypair_for(provider_key: str):
         _class, Provider
     ), "Provider class for {provider} is not an instance of provider.base.Provider"
 
-    setattr(_class, "_provider_key", provider_key)
+    # Set _provider_key attribute of Provider class
+    setattr(_class, "_provider_key", key)
 
-    return (
-        provider_key,
-        {
-            "short_name": short_name,
-            "provider_class": _class,
-            "module_name": module_name,
-        },
+    provider_register[key] = _ProviderInfo(
+        provider_key=key,
+        module_name=mname,
+        short_name=short_name,
+        provider_class=_class,
     )
 
-
-def _get_providers_registery():
-    if not hasattr(_get_providers_registery, "initialized_value"):
-        setattr(
-            get_provider_by_key,
-            "PROVIDES",
-            dict([_keypair_for(p) for p in _PROVIDER_MODULE.keys()]),
-        )
-    return getattr(_get_providers_registery, "PROVIDES")
+    return _class
